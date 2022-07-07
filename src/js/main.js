3f843ec999ca7aa731cc3972ff4d3df3dc496cea
tamasvencel <tamasvenci46@gmail.com>
2022-07-08 00:12:52 +0300

    Latest

--- src/js/main.js
+++ src/js/main.js
@@ -302,3 +302,51 @@ ScrollReveal().reveal(".aboutMeParagraph");
 ScrollReveal().reveal(".techSVG");
 ScrollReveal().reveal(".designSVG");
 ScrollReveal().reveal(".internetSVG", { delay: 300 });
+
+// Navigation
+const nav = document.querySelector(".nav");
+const header = document.querySelector(".headerCyan");
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+//
+
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    // const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    // logo.style.opacity = this;
+  }
+};
+
+//
+
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  nav.classList.add("sticky");
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null, // entire viewport
+  threshold: 0, // when 0% of the header is visible...
+  rootMargin: `-${navHeight + 130}px`,
+});
+headerObserver.observe(header);
